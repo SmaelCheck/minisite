8d67fd9c196b2ad579b0891a00911b6ed4d2f614
Smael CHECK <farid.m.check@gmail.com>
2017-07-28 09:02:31 +0200

    Ajout d'un composant, qui aura comme but gérer tout affichage du pied-de-page

---
+++ src/app/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default class Footer extends React.Component{
+    render(){
+        return(
+            <p>Je suis le contenu du composant Footer.</p>
+        );
+    }
+}

edd62e91b72350b6a1f43368a1c4284792431cab
Smael CHECK <farid.m.check@gmail.com>
2017-07-28 09:02:58 +0200

    Ajout d'un composant, qui aura comme but gérer tout affichage du pied-de-page

---
+++ src/app/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'React';
+
+export default class Main extends React.Component{
+    render(){
+        return(
+            <p>Je suis un cntenu du Main Component</p>
+        );
+    }
+}
